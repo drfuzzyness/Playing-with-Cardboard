5072fd3d4b412a9894e184291b616db3f6ca8e89
Matthew Conto <mbsconto@gmail.com>
2015-02-26 17:34:10 -0500

    Finish

---
+++ Assets/Cardboard/Scripts/moveFromGaze.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveFromGaze : MonoBehaviour {
+
+	public float forceAmt;
+	public CardboardHead cbhead;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		rigidbody.AddForce( cbhead.Gaze.direction.normalized * forceAmt );
+//		Debug.Log( cbhead.Gaze.direction.normalized );
+	}
+}
